8fdedb0dd214dff717611eddfa24bd9944541626
AlonVizel <alon.vizel@gmail.com>
2017-03-22 17:31:11 +0200

    new message

--- app/src/main/java/com/example/apeniazi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apeniazi/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 //Whats going on here
+//hello from Alon
 public class MainActivity extends AppCompatActivity {
 
     @Override
