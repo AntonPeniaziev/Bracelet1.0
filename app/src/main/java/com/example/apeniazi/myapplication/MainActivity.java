4599f95e8f6cc46ae351362e2bbd63d1e8f74f3d
AntonPeniaziev <antonpeniaziev@gmail.com>
2017-03-22 17:12:44 +0200

    test commit

--- app/src/main/java/com/example/apeniazi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apeniazi/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//Whats going on here
 public class MainActivity extends AppCompatActivity {
 
     @Override
